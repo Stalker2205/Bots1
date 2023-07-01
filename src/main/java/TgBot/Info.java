9d050839f1a98caf1abb94696665b88c33de8eb1
stalker2205 <azebus777@mail.ru>
2023-07-01 11:54:04 +0300

    bot1

--- src/main/java/TgBot/Info.java
+++ src/main/java/TgBot/Info.java
@@ -3,6 +3,6 @@ package TgBot;
 import java.util.Stack;
 
 public class Info {
-    public static String Token = "6316293054:AAF8slHiLIx0byHyzlIP2547cplCbQ-kYvc";
+    public static String Token = "";
     public static String BotName = "anixian_bot";
 }
